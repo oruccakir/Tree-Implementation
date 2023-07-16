3efcfcda015071240859180190a1a58294abac06
oruccakir <oruccakir2525@gmail.com>
2023-07-16 13:50:47 +0300

    testing

--- Main.java
+++ Main.java
@@ -5,22 +5,10 @@ public class Main{
     public static void main(String[] args) {
 
         Random rnd = new Random();
+
         
-        TreeSet<Integer> set = new TreeSet<>();
-
-        for(int i=0; i<10; i++) set.add(100+i);
-
-        for(int i=0; i<10; i++) set.add(90+i);
-
-        for(int i=0; i<10; i++) set.add(80+i);
-
-
-       
-
-
-        set.inOrder();
-
-        set.diagonalTraversal();
+        
+        
 
         
 
