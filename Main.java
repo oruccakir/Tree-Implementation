27c9d2e920532ed51547739d5a0231276013e8de
oruccakir <oruccakir2525@gmail.com>
2023-08-17 18:34:42 +0300

    testing

--- Main.java
+++ Main.java
@@ -1,10 +1,23 @@
+import java.util.LinkedList;
 import java.util.Random;
 
 public class Main{
 
     public static void main(String[] args) {
 
-        Random rnd = new Random();
+        int arr[] = {56, 47, 22, 13, 76, 88, 33, 44, 18, 99};
+
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for(int i=0; i<arr.length; i++) set.add(arr[i]);
+
+        set.inOrder();
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        set.convertToLinkedList(set.root, linkedList);
+
+        System.out.println(linkedList);
 
         
         
